40b96466ed12294948edeec0c9c73f2989923da3
Shinzou23 <nino_magsano@dlsu.edu.ph>
2025-02-19 20:04:48 +0800

    commit from windows

--- main.js
+++ main.js
@@ -1,96 +1,103 @@
-const time = document.getElementById('time'),
-    greeting = document.getElementById('greeting'),
-    name = document.getElementById('name'),
-    focus = document.getElementById('focus'),
-    content = document.getElementById('content'),
-    author = document.getElementById('author');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { LocalStorage } = require('node-localstorage');
+
+const dom = new JSDOM(`<!DOCTYPE html><body><div id="time"></div><div id="greeting"></div><div id="name"></div><div id="focus"></div><div id="content"></div><div id="author"></div></body>`, {
+  url: "http://localhost"
+});
+
+global.window = dom.window;
+global.document = dom.window.document;
+global.localStorage = new LocalStorage('./scratch');
+
+// Mocks
+global.Swal = {
+  mixin: () => ({
+    queue: () => Promise.resolve({ value: ['Test Name', 'Test Focus'] }),
+    fire: () => {}
+  })
+};
+
+global.axios = {
+  get: () => Promise.resolve({
+    data: {
+      content: 'Test quote content',
+      author: 'Test author'
+    }
+  })
+};
+
+// Your existing code starts here
+const time = document.getElementById('time');
+const greeting = document.getElementById('greeting');
+const name = document.getElementById('name');
+const focus = document.getElementById('focus');
+const content = document.getElementById('content');
+const author = document.getElementById('author');
 
 function showTime() {
-    let today = new Date(),
-        hour = today.getHours(),
-        min = today.getMinutes(),
-        sec = today.getSeconds();
+  let today = new Date(),
+      hour = today.getHours(),
+      min = today.getMinutes(),
+      sec = today.getSeconds();
 
-    const amPm = hour >= 12 ? 'PM' : 'AM';
+  const amPm = hour >= 12 ? 'PM' : 'AM';
+  hour = hour % 12 || 12;
 
-    hour = hour % 12 || 12;
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${amPm}`;
 
-    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${amPm}`;
-
-    setTimeout(showTime, 1000);
+  setTimeout(showTime, 1000);
 }
 
 function addZero(n) {
-    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+  return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
 function setBg() {
-    let hour = new Date().getHours();
+  let hour = new Date().getHours();
 
-    if (hour < 12) {
-        document.body.style.backgroundImage = 'url(images/morning.jpg)';
-        greeting.textContent = 'Good Morning, ';
-    } else if (hour < 18) {
-        document.body.style.backgroundImage = 'url(images/afternoon.jpg)';
-        greeting.textContent = 'Good Afternoon, ';
-    } else {
-        document.body.style.backgroundImage = 'url(images/night.jpg)';
-        greeting.textContent = 'Good Evening, ';
-        document.body.style.color = 'white';
-    }
+  if (hour < 12) {
+    greeting.textContent = 'Good Morning, ';
+  } else if (hour < 18) {
+    greeting.textContent = 'Good Afternoon, ';
+  } else {
+    greeting.textContent = 'Good Evening, ';
+  }
 }
 
 function getName() {
-    if (localStorage.getItem('name') === null) {
-        name.textContent = '[Enter Name]';
-    } else {
-        name.textContent = localStorage.getItem('name');
-    }
+  if (localStorage.getItem('name') === null) {
+    name.textContent = '[Enter Name]';
+  } else {
+    name.textContent = localStorage.getItem('name');
+  }
 }
 
 function getFocus() {
-    if (localStorage.getItem('focus') === null) {
-        focus.textContent = '[Enter Focus]';
-    } else {
-        focus.textContent = localStorage.getItem('focus');
-    }
+  if (localStorage.getItem('focus') === null) {
+    focus.textContent = '[Enter Focus]';
+  } else {
+    focus.textContent = localStorage.getItem('focus');
+  }
 }
 
 if (localStorage.getItem('name') === null && localStorage.getItem('focus') === null) {
-    Swal.mixin({
-        input: 'text',
-        confirmButtonText: 'Next &rarr;',
-        progressSteps: ['1', '2']
-    }).queue([
-        {
-            title: 'Your Name'
-        },
-        {
-            focus: 'Your Focus'
-        }
-    ]).then((result) => {
-        if (result.value) {
-            Swal.fire({
-                title: 'All done!',
-                html: `
-              <p>Your Name: ${result.value[0]}</p>
-              <p>Your Focus: ${result.value[1]}</p>
-            `,
-                confirmButtonText: 'Lovely!'
-            })
-
-            localStorage.setItem('name', result.value[0]);
-            localStorage.setItem('focus', result.value[1]);
-        }
-    })
+  Swal.mixin({}).queue([]).then((result) => {
+    if (result.value) {
+      localStorage.setItem('name', result.value[0]);
+      localStorage.setItem('focus', result.value[1]);
+    }
+  });
 }
 
 axios.get('https://api.quotable.io/random').then(res => {
-    content.textContent = res.data.content;
-    author.textContent = res.data.author;
+  content.textContent = res.data.content;
+  author.textContent = res.data.author;
 });
 
 showTime();
 setBg();
 getName();
 getFocus();
+
+console.log('Script executed successfully');
